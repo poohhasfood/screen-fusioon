c26f2c10a2f4075f7d6696554a0aefa041a5c672
poohhasfood <151662490+poohhasfood@users.noreply.github.com>
2024-04-15 12:02:13 +0530

    Add files via upload

---
+++ screen-fusion/src/main/java/code/crafters/screen/fusion/ScreenFusionApplication.java
@@ -0,0 +1,13 @@
+package code.crafters.screen.fusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenFusionApplication.class, args);
+	}
+
+}
---
+++ screen-fusion/src/test/java/code/crafters/screen/fusion/ScreenFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package code.crafters.screen.fusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
